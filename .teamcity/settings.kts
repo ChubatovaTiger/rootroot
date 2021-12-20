73c570bedaca3bf70e268a7d7abdbfdb1c91e29c
78346593+chubatovatiger <78346593+chubatovatiger@TeamCity>
2021-12-20 14:33:14 +0100

    TeamCity change in '74411' project: 'https://github.com/ChubatovaTiger/commitstatuses' VCS root was updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -48,11 +48,11 @@ object id74411 : BuildType({
 
 object HttpsGithubComChubatovaTigerCommitstatuses : GitVcsRoot({
     name = "https://github.com/ChubatovaTiger/commitstatuses"
-    url = "git@github.com:ChubatovaTiger/commitstatuses.git"
+    url = "https://github.com/ChubatovaTiger/commitstatuses"
     branch = "%branch_name%"
     branchSpec = "+:refs/heads/*"
-    authMethod = uploadedKey {
-        uploadedKey = "id_rsa_tc_tiger"
-        passphrase = "credentialsJSON:c6ac2c5c-1b3d-44c0-bd98-04d75dda453a"
+    authMethod = password {
+        userName = "ChubatovaTiger"
+        password = "credentialsJSON:6d0e3a4f-5b43-4ca9-adba-25e4e4e8b8a4"
     }
 })
